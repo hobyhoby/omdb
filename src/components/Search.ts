083603b210610af3b131522770fd66b3db0f7d90
hobyhoby <hoby@nate.com>
2023-03-15 15:19:39 +0900

    Compiled Typescript

--- src/components/Search.ts
+++ src/components/Search.ts
@@ -5,28 +5,32 @@ export default class Search extends Component {
   render() {
     this.el.classList.add("search");
     this.el.innerHTML = /* html */ `
-      <input 
-      value="${movieStore.state.searchText}" 
-      placeholder="Enter the movie title to search!"/>
+      <input
+          value="${movieStore.state.searchText}" 
+          placeholder="Enter the movie title to search!" />
       <button class="btn btn-primary">
         Search!
       </button>
     `;
 
     const inputEl = this.el.querySelector("input");
-    inputEl.addEventListener("input", () => {
+    inputEl?.addEventListener("input", () => {
       movieStore.state.searchText = inputEl.value;
     });
-    inputEl.addEventListener("keydown", (event) => {
-      if (event.key === "Enter" && movieStore.state.searchText.trim()) {
-        searchMovies(1);
+    inputEl?.addEventListener("keydown", async (event: Event) => {
+      if (
+        event instanceof KeyboardEvent &&
+        event.key === "Enter" &&
+        movieStore.state.searchText.trim()
+      ) {
+        await searchMovies(1);
       }
     });
 
     const btnEl = this.el.querySelector(".btn");
-    btnEl.addEventListener("click", () => {
+    btnEl?.addEventListener("click", async () => {
       if (movieStore.state.searchText.trim()) {
-        searchMovies(1);
+        await searchMovies(1);
       }
     });
   }
